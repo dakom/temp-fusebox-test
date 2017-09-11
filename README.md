1412040050179aa3e7d4d2a8dde4709d4f461cb2
David Komer <david.komer@gmail.com>
2017-09-11 11:16:50 +0300

    worker fixed

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 # Workers (current master)
 
-Shows workers failing
-
-1. `npm run build`
-2. Open console (note - it's using http-server not dev server, you may need to refresh)
-3. `ReferenceError: global is not defined`
-
-To demonstrate it is not a code error:
-
-4. Keep the browser open as-is
-5. Copy-paste the `worker.ts` source exactly to replace the generated `dist/worker.js`
-6. Refresh and look in the console - you'll see a successful roundtrip communication
+Now fixed! Requires using Quantum and containedAPI
 
 # Timeout branch
 
--- fuse.js
+++ fuse.js
@@ -1,4 +1,4 @@
-const {WebIndexPlugin, FuseBox} = require("fuse-box");
+const {WebIndexPlugin, QuantumPlugin, FuseBox} = require("fuse-box");
 
 const mainProducer = FuseBox.init({
     homeDir: "src",
@@ -15,15 +15,26 @@ const mainProducer = FuseBox.init({
 
 const mainBundle = mainProducer.bundle("main");
 mainBundle.instructions(`>Main.ts`);
-mainProducer.run();
+mainBundle.watch();
+mainProducer.dev({ open: true });
 
 
 const workerProducer = FuseBox.init({
     homeDir: "src",
     output: `dist/$name.js`,
-    target: "browser"
+    target: "browser",
+    plugins: [
+        QuantumPlugin({
+            containedAPI : true,
+            bakeApiIntoBundle: "worker",
+            target : "server"
+        })
+    ]
 });
 
 const workerBundle = workerProducer.bundle("worker");
-workerBundle.instructions(`Worker.ts`);
+workerBundle.watch();
+workerBundle.instructions(`>Worker.ts`);
+
 workerProducer.run();
+mainProducer.run();
